d365210c58d7612473db60a6cfbd17e029a74a8e
positive132 <nekit_ich@tutamail.com>
2026-03-05 16:15:08 +0300

    lz_1.py

--- lz_1.py
+++ lz_1.py
@@ -1,41 +1,61 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 p = 3.14
-
 length = float(input('Введите длину стороны октагона:'))
 
 class Octagon: 
-    def __init__(self, length:int):
-        self._length = length
+    def __init__(self, length: float):
+        self.length = length
         self.k = 1 + 2 ** 0.5 
         self.ugle = 3 * p / 4 
 
-def opis (length: float):
+    def opis (self):
 
-    R = length / 2 * (4 + 2 * (2) ** 0.5) ** 0.5
-    area1 = p*(R) ** 2 
+        R = self.length / 2 * (4 + 2 * (2) ** 0.5) ** 0.5
+        area1 = p*(R) ** 2 
 
-    return R, area1
+        return R, area1
 
-def vpis (length: float):
+    def vpis (self):
 
-    r = length/2 * (1 + (2) ** 0.5)
-    area2 = p*(r)**2 
+        r = self.length / 2 * (1 + (2) ** 0.5)
+        area2 = p*(r)**2 
 
-    return r, area2
+        return r, area2
 
-def octagonperimpl(length: float):
+    def perimpl(self):
 
-    perimetr = 8 * length
-    area3 = 2 * (1 + 2 ** 0.5) * (length ** 2)
+        perimetr = 8 * self.length
+        area3 = 2 * (1 + 2 ** 0.5) * (self.length ** 2)
 
-    return perimetr, area3
+        return perimetr, area3
+    def graf(self):
 
+        R = length / 2 * (4 + 2 * (2) ** 0.5) ** 0.5
+        ugles = np.linspace(0, 2 * p, 9) + p / 8
+        r = length / 2 * (1 + (2) ** 0.5)
+        circle_ugles = np.linspace(0, 2 * p, 100)
+
+        x = R * np.cos(ugles)
+        y = R * np.sin(ugles)
+
+        plt.figure(figsize=(5, 5))
+
+        plt.plot(R * np.cos(circle_ugles), R * np.sin(circle_ugles),'y', lw = 2.5)
+        
+        plt.plot(r * np.cos(circle_ugles), r * np.sin(circle_ugles),'r', lw = 2.5)
+        plt.plot(x, y, 'b-o', lw = 2.5) 
+
+        plt.show()
 
 def main():
-    print('радиус и площадь описанной окружности:', opis(length))
-    print('радиус и площадь вписанной окружности:', vpis(length))
-    print('периметр и площадь октагона:', octagonperimpl(length)) 
+    octagon = Octagon(length)
+    print('радиус и площадь описанной окружности:',octagon.opis() )
+    print('радиус и площадь вписанной окружности:',octagon.vpis())
+    print('периметр и площадь октагона:', octagon.perimpl()) 
+    octagon.graf()
     
 if __name__ == '__main__':
     main()    
